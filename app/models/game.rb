de6128138cc0d5dfd7947fb0f302aef37e294056
TracyAtieno <tracy.atieno@student.moringaschool.com>
2023-03-04 02:12:37 +0300

    completed-assignment

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,5 @@ class Game < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
 
-  def games
-    sql = <<-SQL
-    SELECT * FROM games
-   SQL
-  DB[:conn].execute(sql).map do |game|
-      Game.new(...)
-  end
- end
+
 end
