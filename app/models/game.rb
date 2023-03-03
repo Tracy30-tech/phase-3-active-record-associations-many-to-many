60d8758c3bb37a0384c557ad8a1601c7697db274
TracyAtieno <tracy.atieno@student.moringaschool.com>
2023-03-04 02:05:39 +0300

    many-to-many-relationship

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,13 @@
 class Game < ActiveRecord::Base
   has_many :reviews
+  has_many :users, through: :reviews
+
+  def games
+    sql = <<-SQL
+    SELECT * FROM games
+   SQL
+  DB[:conn].execute(sql).map do |game|
+      Game.new(...)
+  end
+ end
 end
